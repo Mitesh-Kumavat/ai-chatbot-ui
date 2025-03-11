8f378a7b47f76c33795750c01372999e07abd196
Mitesh-Kumavat <miteshkumavat777@gmail.com>
2025-03-11 13:11:10 +0530

    Send button validation

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -100,14 +100,14 @@ const InputBox: React.FC<InputBoxProps> = ({ onSend, selectedLanguage }) => {
                         }
                     </button>
 
-                    <button
+                    {(message !== "") && <button
                         onClick={handleSend}
                         disabled={isRecording || message === ""}
                         className={`text-xl text-white hover:bg-zinc-800 p-2 bg-zinc-900 rounded-full text-center items-center  
                     ${(isRecording || message === "") && "cursor-not-allowed"}`}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="text-center lucide lucide-send"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z" /><path d="m21.854 2.147-10.94 10.939" /></svg>
-                    </button>
+                    </button>}
                 </div>
             </div>
         </div>
